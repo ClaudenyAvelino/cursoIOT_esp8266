f799788bd311e12545ee68b47ffb37dfb0e2c53d
claudeny.avelino <claudeny.avelino@hotmail.com>
2025-04-09 10:28:27 -0300

    Adicionando led.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 
+const byte LED = 2;
+
 void setup() {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   Serial.println("Low Voltage");
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED, LOW);
   delay(1000);
   Serial.println("High Voltage");
-  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED, HIGH);
   delay(1000);
 }
